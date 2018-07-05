db6ac24b0b4c70f84caa8b3c82d59e0a07e449c4
Rafał Rudol <rrrrudol@gmail.com>
2018-07-05 19:47:02 +0200

    improvements

---
+++ README.md
@@ -0,0 +1,15 @@
+## How to use
+Provide proper configuration in `server.js` and `client.js` files.
+
+Run `server.js` on host machine.
+
+Run `client.js` on user machine and provide command as a argument.
+```
+node client.js ls
+```
+
+You can also pass arguments:
+```
+node client.js ls -l
+```
+
--- client.js
+++ client.js
@@ -6,11 +6,25 @@ function rsh(command, input) {
   input = Buffer.from(input, 'utf8');
   function callback(err, res) {
     if (err) { console.log(err); }
-    console.log(res);
+    console.log(res.result);
+    process.exit(0);
   }
   client.call({"jsonrpc": "2.0", "method": "sh", "params": { command, input }, "id": 0}, callback);  
 }
 
-rsh('grep world', `hello world
-test abc
-world of tests`);
+let data = '';
+const command = process.argv.slice(2).join(' ');
+
+// process.stdin.resume();
+process.stdin.setEncoding('utf8');
+
+let timeout = setTimeout(() => rsh(command, ''), 100);
+
+process.stdin.on('data', (chunk) => {
+  clearTimeout(timeout);
+  data += chunk;
+});
+
+process.stdin.on('end', () => {
+  rsh(command, data);
+});
